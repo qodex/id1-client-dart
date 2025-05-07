7ccaa00597b33a5b01332ae3c561bfc49a2e3d91
Max <m@id1.au>
2025-05-07 11:13:35 +1000

    RC v1.0.0

--- test/id1_client_test.dart
+++ test/id1_client_test.dart
@@ -4,6 +4,9 @@ import 'package:id1_client/src/key.dart';
 import 'package:id1_client/src/rsa.dart';
 import 'package:test/test.dart';
 
+var fakepk =
+    "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBALeAzDnMCy8G4n6zB1VbwRz2vXO7jZn6btOyChpXrJqznyHGs8J4FSg2EWbPfzTIGleMHuPBfV/mcatkD0Va3aGa0k0sK+vpTVkxl4RFs9aMUgTTYFYrSn6pkoHKOk2/4tgyvxgz8l9iaqGFVuGqejXm9UXJ82ZpUgHrLXIm2fvFAgMBAAECgYA7WIkz6/x18gkQJsApZ+o1fsrYggCSmr568mp4CIDG6258kvGR5Bobjhjkohimrkxuod9fkzUD7dg3ML4LlQ5150KqabXQj+D9wpmTdbYdVzLsvWS9P4AEnuLI1QtHnExKlH630k+e+bVeFFn0Ys455vCzbNMVoOpj6J2ZMPHu5QJBAPGRag7wm6o/4wTGG5r+G4MrzLWMHCz9rNLRQtBHP6Ibnrg1eB8hURJeDtmEYMTnWuBASp6aY1wbCV/gv8aZhVcCQQDCd1P+Xmv+cGAWuQKKidNF7JgipHkizCwKez5TgkKHfSZQHWyEw+YvPYE4reKc/+0kRHSAdRzZ1VT0jlhD4NpDAkEAvC4oLZ031mdQRQ7CwHnFGujK3n5YEKBIui2z26Y1JuZXPW7BtZZxnlpRPRdHfvwvKdRLjMP+NOkG436kRmSfnwJAREblRLAeNq1570Bx9fZCKZDSMYeRyHfrFpsC3QslFLEKHKLYER0+2mM71YynvfvVZSBrzxZPVOQj+eFoeBygRQJBANe6x0ddXK8iJ65lOmiGAvW4mghHZEryxDyH5wc3C2dHIMZPFXO/B4JjUGq3IXNHTv34jewUVWRRIcxU+y4bdMU=";
+
 void main() {
   group('id1 client tests', () {
     setUp(() {
@@ -24,7 +27,9 @@ void main() {
       var encB64 =
           "aevZivMEyYezaxswyo4cZuFe2IobP6Ba0R9Tm51mQ1NEclciuO4z5+27zF2cT6S+jW6TK7C5rhioSvqSV+f1a3XSfCyZZAtTc6n5TxoAa+hJauS1m3H6PSq9SNYahstQV/RRZt70jyu0Jj+GIr+Q49nw57xHIv6nYDIZVkhmpjk=";
       var enc = base64Decode(encB64);
-      var dec = Utf8Decoder(allowMalformed: true).convert(RSA.decrypt(enc, RSA.parsePrivateKeyPEM(prv)));
+      var dec = Utf8Decoder(
+        allowMalformed: true,
+      ).convert(RSA.decrypt(enc, RSA.parsePrivateKeyPEM("$keyPrefix\n$fakepk\n$keySuffix")));
       expect(dec, "/j0G7IInr7FusMKfbkW6Sy7mM7bLpcIK16u1V+uZRyE=");
     });
 
@@ -37,6 +42,5 @@ void main() {
   });
 }
 
-var prv = """-----BEGIN PRIVATE KEY-----
-MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBALeAzDnMCy8G4n6zB1VbwRz2vXO7jZn6btOyChpXrJqznyHGs8J4FSg2EWbPfzTIGleMHuPBfV/mcatkD0Va3aGa0k0sK+vpTVkxl4RFs9aMUgTTYFYrSn6pkoHKOk2/4tgyvxgz8l9iaqGFVuGqejXm9UXJ82ZpUgHrLXIm2fvFAgMBAAECgYA7WIkz6/x18gkQJsApZ+o1fsrYggCSmr568mp4CIDG6258kvGR5Bobjhjkohimrkxuod9fkzUD7dg3ML4LlQ5150KqabXQj+D9wpmTdbYdVzLsvWS9P4AEnuLI1QtHnExKlH630k+e+bVeFFn0Ys455vCzbNMVoOpj6J2ZMPHu5QJBAPGRag7wm6o/4wTGG5r+G4MrzLWMHCz9rNLRQtBHP6Ibnrg1eB8hURJeDtmEYMTnWuBASp6aY1wbCV/gv8aZhVcCQQDCd1P+Xmv+cGAWuQKKidNF7JgipHkizCwKez5TgkKHfSZQHWyEw+YvPYE4reKc/+0kRHSAdRzZ1VT0jlhD4NpDAkEAvC4oLZ031mdQRQ7CwHnFGujK3n5YEKBIui2z26Y1JuZXPW7BtZZxnlpRPRdHfvwvKdRLjMP+NOkG436kRmSfnwJAREblRLAeNq1570Bx9fZCKZDSMYeRyHfrFpsC3QslFLEKHKLYER0+2mM71YynvfvVZSBrzxZPVOQj+eFoeBygRQJBANe6x0ddXK8iJ65lOmiGAvW4mghHZEryxDyH5wc3C2dHIMZPFXO/B4JjUGq3IXNHTv34jewUVWRRIcxU+y4bdMU=
------END PRIVATE KEY-----""";
+var keyPrefix = "-----BEGIN PRIVATE KEY-----";
+var keySuffix = "-----END PRIVATE KEY-----";
